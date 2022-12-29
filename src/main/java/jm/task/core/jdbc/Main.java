23f9be94a4872cc5eaf52d94a0543e9d8fec5a5c
Albert Dmitriev <albertdmitriev94@mail.ru>
2022-12-29 19:07:51 +0300

    final_project

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Polly", "Dangerous", (byte) 23);
+        userDao.saveUser("Jack", "Sparrow", (byte) 45);
+        userDao.saveUser("Bruce", "Wayne", (byte) 40);
+        userDao.saveUser("Peter", "Parker", (byte) 25);
+
+        userDao.removeUserById(4);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
+
